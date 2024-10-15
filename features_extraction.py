5b2028b762959e90bffca0311b8cea12e56dafc9
MOHAMMED SHAMIL ABBAS <107237717+mohammedshamilgithubexpert@users.noreply.github.com>
2024-10-16 01:29:59 +0500

    Update features_extraction.py

--- features_extraction.py
+++ features_extraction.py
@@ -112,4 +112,218 @@ def request_url(wiki, soup, domain):
             success = success + 1
         i = i + 1
 
-    for embed in soup.find_all
+    for embed in soup.find_all ('embed', src=True):
+        dots = [x.start() for x in re.finditer(r'\.', embed['src'])]
+        if wiki in embed['src'] or domain in embed['src'] or len(dots) == 1:
+            success = success + 1
+        i = i + 1
+
+    for i_frame in soup.find_all('i_frame', src=True):
+        dots = [x.start() for x in re.finditer(r'\.', i_frame['src'])]
+        if wiki in i_frame['src'] or domain in i_frame['src'] or len(dots) == 1:
+            success = success + 1
+        i = i + 1
+
+    try:
+        percentage = success / float(i) * 100
+    except:
+        return 1
+
+    if percentage < 22.0:
+        return 1
+    elif 22.0 <= percentage < 61.0:
+        return 0
+    else:
+        return -1
+
+def url_of_anchor(wiki, soup, domain):
+    i = 0
+    unsafe = 0
+    for a in soup.find_all('a', href=True):
+        # 2nd condition was 'JavaScript ::void(0)' but we put JavaScript because the space between javascript and ::
+        # might not be there in the actual a['href']
+        if "#" in a['href'] or "javascript" in a['href'].lower() or "mailto" in a['href'].lower() or not (
+                wiki in a['href'] or domain in a['href']):
+            unsafe = unsafe + 1
+        i = i + 1
+        # print a['href']
+    try:
+        percentage = unsafe / float(i) * 100
+    except:
+        return 1
+    if percentage < 31.0:
+        return 1
+        # return percentage
+    elif 31.0 <= percentage < 67.0:
+        return 0
+    else:
+        return -1
+
+def links_in_tags(wiki, soup, domain):
+    i = 0
+    success = 0
+    for link in soup.find_all('link', href=True):
+        dots = [x.start() for x in re.finditer(r'\.', link['href'])]
+        if wiki in link['href'] or domain in link['href'] or len(dots) == 1:
+            success = success + 1
+        i = i + 1
+
+    for script in soup.find_all('script', src=True):
+        dots = [x.start() for x in re.finditer(r'\.', script['src'])]
+        if wiki in script['src'] or domain in script['src'] or len(dots) == 1:
+            success = success + 1
+        i = i + 1
+    try:
+        percentage = success / float(i) * 100
+    except:
+        return 1
+
+    if percentage < 17.0:
+        return 1
+    elif 17.0 <= percentage < 81.0:
+        return 0
+    else:
+        return -1
+
+def sfh(wiki, soup, domain):
+    for form in soup.find_all('form', action=True):
+        if form['action'] == "" or form['action'] == "about:blank":
+            return -1
+        elif wiki not in form['action'] and domain not in form['action']:
+            return 0
+        else:
+            return 1
+    return 1
+
+def submitting_to_email(soup):
+    for form in soup.find_all('form', action=True):
+        return -1 if "mailto:" in form['action'] else 1
+    # In case there is no form in the soup, then it is safe to return 1.
+    return 1
+
+def abnormal_url(domain, url):
+    hostname = domain.name
+    match = re.search(hostname, url)
+    return 1 if match else -1
+
+def i_frame(soup):
+    for i_frame in soup.find_all('i_frame', width=True, height=True, frameBorder=True):
+        # Even if one iFrame satisfies the below conditions, it is safe to return -1 for this method.
+        if i_frame['width'] == "0" and i_frame['height'] == "0" and i_frame['frameBorder'] == "0":
+            return -1
+        if i_frame['width'] == "0" or i_frame['height'] == "0" or i_frame['frameBorder'] == "0":
+            return 0
+    # If none of the iframes have a width or height of zero or a frameBorder of size 0, then it is safe to return 1.
+    return 1
+
+def age_of_domain(domain):
+    creation_date = domain.creation_date
+    expiration_date = domain.expiration_date
+    ageof domain = abs((expiration_date - creation_date).days)
+    return -1 if ageofdomain / 30 < 6 else 1
+
+def dns(domain):
+    try:
+        result = dns.resolver.resolve(domain, 'A')
+        return 1
+    except:
+        return -1
+
+def web_traffic(url):
+    try:
+        rank = bs4.BeautifulSoup(urllib.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), "xml").find(
+            "REACH")['RANK']
+    except TypeError:
+        return -1
+    rank = int(rank)
+    return 1 if rank < 100000 else 0
+
+def google_index(url):
+    site = search(url, 5)
+    return 1 if site else -1
+
+def statistical_report(url, hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+    except:
+        return -1
+    url_match = re.search(
+        r'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly', url)
+    ip_match = re.search(
+        '146\.112\.61\.108|213\.174\.157\.151|121\.50\.168\.88|192\.185\.217\.116|78\.46\.211\.158|181\.174\.165\.13|46\.242\.145\.103|121\.50\.168\.40|83\.125\.22\.219|46\.242\.145\.98|'
+        '107\.151\.148\.44|107\.151\.148\.107|64\.70\.19\.203|199\.184\.144\.27|107\.151\.148\.108|107\.151\.148\.109|119\.28\.52\.61|54\.83\.43\.69|52\.69\.166\.231|216\.58\.192\.225|'
+        '118\.184\.25\.86|67\.208\.74\.71|23\.253\.126\.58|104\.239\.157\.210|175\.126\.123\.219|141\.8\.224\.221|10\.10\.10\.10|43\.229\.108\.32|103\.232\.215\.140|69\.172\.201\.153|'
+        '216\.218\.185\.162|54\.225\.104\.146|103\.243\.24\.98|199\.59\.243\.120|31\.170\.160\.61|213\.19\.128\.77|62\.113\.226\.131|208\.100\.26\.234|195\.16\.127\.102|195\.16\.127\.157|'
+        '34\.196\.13\.28|103\.224\.212\.222|172\.217\.4\.225|54\.72\.9\.51|192\.64\.147\.141|198\.200\.56\.183|23\.253\.164\.103|52\.48\.191\.26|52\.214\.197\.72|87\.98\.255\.18|209\.99\.17\.27|'
+        '216\.38\.62\.18|104\.130\.124\.96|47\.89\.58\.141|78\.46\.211\.158|54\.86\.225\.156|54\.82\.156\.19|37\.157\.192\.102|204\.11\.56\.48',
+        ip_address)
+    if url_match:
+        return -1
+    elif ip_match:
+        return -1
+    else:
+        return 1
+
+def get_hostname_from_url(url):
+    hostname = url
+    pattern = "https://|http://|www.|https://www.|http://www."
+    pre_pattern_match = re.search(pattern, hostname)
+
+    if pre_pattern_match:
+        hostname = hostname[pre_pattern_match.end():]
+        post_pattern_match = re.search("/", hostname)
+        if post_pattern_match:
+            hostname = hostname[:post_pattern_match.start()]
+
+    return hostname
+
+def main(url):
+    with open(LOCALHOST_PATH + DIRECTORY_NAME + '/markup.txt', 'r') as file:
+        soup_string = file.read()
+
+    soup = BeautifulSoup(soup_string, 'html.parser')
+
+    status = []
+    hostname = get_hostname_from_url(url)
+
+    status.append(having_ip_address(url))
+    status.append(url_length(url))
+    status.append(shortening_service(url))
+    status.append(having_at_symbol(url))
+    status.append(double_slash_redirecting(url))
+    status.append(prefix_suffix(hostname))
+    status.append(having_sub_domain(url))
+
+    dns = 1
+    try:
+        domain = whois.query(hostname)
+    except:
+        dns = -1
+
+    status.append(favicon(url, soup, hostname))
+    status.append(https_token(url))
+    status.append(request_url(url, soup, hostname))
+    status.append(url_of_anchor(url, soup, hostname))
+    status.append(links_in_tags(url, soup, hostname))
+    status.append(sfh(url, soup, hostname))
+    status.append(submitting_to_email(soup))
+
+    status.append(-1 if dns == -1 else abnormal_url(domain, url))
+
+    status.append(i_frame(soup))
+
+    status.append(-1 if dns == -1 else age_of_domain(domain))
+
+    status.append(dns)
+
+    status.append(web_traffic(url))
+    status.append(google_index(url))
+    status.append(statistical_report(url, hostname))
+
+    return status
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please use the following format for the command - `python2 features_extraction.py <url-to-be-tested>`")
+        exit(0)
+    main(sys.argv[1])
