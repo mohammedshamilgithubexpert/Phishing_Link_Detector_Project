eb8c1f96a708fabc124024e28e7f320e3ab5d4ff
MOHAMMED SHAMIL ABBAS <107237717+mohammedshamilgithubexpert@users.noreply.github.com>
2024-10-16 00:38:01 +0500

    Create patterns.py

---
+++ patterns.py
@@ -0,0 +1,6 @@
+# Purpose - This file contains the patterns used for feature extraction.
+
+ipv4_pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+ipv6_pattern = r"^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$"
+shortening_services = r"bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|"
+http_https = r"https://|http://"
